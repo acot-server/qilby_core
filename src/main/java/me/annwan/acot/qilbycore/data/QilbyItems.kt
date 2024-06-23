ef56bac40f286008a8867e78633c00d1edb999a7
LunaFreyjaNox <romain.caudy@gmail.com>
2024-06-23 21:53:32 +0200

    Add Phenomenal Super-Capacitor (UIV); Stellarite Super-Capacitor(UXV), Éliacube (T1 OpV) Éliasphère (T2 Opv + replacement of ultimate battery) Add loc and texture

--- src/main/java/me/annwan/acot/qilbycore/data/QilbyItems.kt
+++ src/main/java/me/annwan/acot/qilbycore/data/QilbyItems.kt
@@ -39,6 +39,58 @@ object QilbyItems {
                 )
             }
             .register()
+    @Suppress("UNUSED")
+    val BATTERY_PHI_SUPER_CAPACITOR : ItemEntry<ComponentItem> =
+        createItem("phi_super_capacitor", ComponentItem::create)
+            .lang("Phenomenal Super-capacitor")
+            .onRegister { item: ComponentItem ->
+                item.attachComponents(
+                    ElectricStats.createRechargeableBattery(
+                        1_280_000_000_000,
+                        GTValues.UIV
+                    )
+                )
+            }
+            .register()
+    @Suppress("UNUSED")
+    val BATTERY_SIGMA_SUPER_CAPACITOR : ItemEntry<ComponentItem> =
+        createItem("sigma_super_capacitor", ComponentItem::create)
+            .lang("Stellarite Infused Super-capacitor")
+            .onRegister { item: ComponentItem ->
+                item.attachComponents(
+                    ElectricStats.createRechargeableBattery(
+                        5_120_000_000_000,
+                        GTValues.UXV
+                    )
+                )
+            }
+            .register()
+    @Suppress("UNUSED")
+    val ELIACUBE : ItemEntry<ComponentItem> =
+        createItem("eliacube_capacitor", ComponentItem::create)
+            .lang("Eliacube")
+            .onRegister { item: ComponentItem ->
+                item.attachComponents(
+                    ElectricStats.createRechargeableBattery(
+                        20_480_000_000_000,
+                        GTValues.OpV
+                    )
+                )
+            }
+            .register()
+    @Suppress("UNUSED")
+    val ELIASPHERE : ItemEntry<ComponentItem> =
+        createItem("eliasphere_capacitor", ComponentItem::create)
+            .lang("Eliasphere")
+            .onRegister { item: ComponentItem ->
+                item.attachComponents(
+                    ElectricStats.createRechargeableBattery(
+                        9_000_000_000_000_000_000,
+                        GTValues.OpV
+                    )
+                )
+            }
+            .register()
 
     private fun <T : Item> createItem(
         name: String,
