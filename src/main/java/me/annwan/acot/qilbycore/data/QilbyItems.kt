bf4403bfb7137501aea7d1b905aa49950e8d0493
LunaFreyjaNox <56685367+LunaFreyjaNox@users.noreply.github.com>
2025-01-02 22:17:16 +0100

    update version add lot of new circuits, wafer change some textures

--- src/main/java/me/annwan/acot/qilbycore/data/QilbyItems.kt
+++ src/main/java/me/annwan/acot/qilbycore/data/QilbyItems.kt
@@ -1,5 +1,4 @@
 package me.annwan.acot.qilbycore.data
-
 import com.gregtechceu.gtceu.api.GTValues
 import com.gregtechceu.gtceu.api.item.ComponentItem
 import com.gregtechceu.gtceu.api.item.component.ElectricStats
@@ -26,6 +25,7 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val BATTERY_ENIGMATIC_SUPER_CAPACITOR : ItemEntry<ComponentItem> =
         createItem("enigmatic_super_capacitor", ComponentItem::create)
@@ -39,6 +39,7 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val BATTERY_PHI_SUPER_CAPACITOR : ItemEntry<ComponentItem> =
         createItem("phi_super_capacitor", ComponentItem::create)
@@ -52,6 +53,7 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val BATTERY_SIGMA_SUPER_CAPACITOR : ItemEntry<ComponentItem> =
         createItem("sigma_super_capacitor", ComponentItem::create)
@@ -65,6 +67,7 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val ELIACUBE : ItemEntry<ComponentItem> =
         createItem("eliacube_capacitor", ComponentItem::create)
@@ -78,6 +81,7 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val ELIASPHERE : ItemEntry<ComponentItem> =
         createItem("eliasphere_capacitor", ComponentItem::create)
@@ -91,45 +95,161 @@ object QilbyItems {
                 )
             }
             .register()
+
     @Suppress("UNUSED")
     val SHUKRUTE_CIRCUIT_BOARD : ItemEntry<ComponentItem> =
         createItem("shukrute_circuit_board", ComponentItem::create)
             .lang("Shukrute Circuit Board")
             .register()
+
     @Suppress("UNUSED")
     val SHUKRUTE_PRINTED_CIRCUIT_BOARD : ItemEntry<ComponentItem> =
         createItem("shukrute_printed_circuit_board", ComponentItem::create)
             .lang("Shukrute Printed Circuit Board")
             .register()
+
     @Suppress("UNUSED")
     val RUSHU_CIRCUIT_BOARD : ItemEntry<ComponentItem> =
         createItem("rushu_circuit_board", ComponentItem::create)
             .lang("Rushu Circuit Board")
             .register()
+
     @Suppress("UNUSED")
     val RUSHU_PRINTED_CIRCUIT_BOARD : ItemEntry<ComponentItem> =
         createItem("rushu_printed_circuit_board", ComponentItem::create)
             .lang("Rushu Printed Circuit Board")
             .register()
+
     @Suppress("UNUSED")
     val SHUSHU_CIRCUIT_MAX : ItemEntry<ComponentItem> =
         createItem("shushu_circuit_max", ComponentItem::create)
             .lang("MAX Shushu holder")
             .register()
+
     @Suppress("UNUSED")
     val UNSOLDERED_LV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
         createItem("unsoldered_lv_circuit_assembly", ComponentItem::create)
             .lang("Unsoldered LV Circuit Assembly")
             .register()
+
     @Suppress("UNUSED")
     val LV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
         createItem("lv_circuit_assembly", ComponentItem::create)
             .lang("LV Circuit Assembly")
             .register()
+
     @Suppress("UNUSED")
     val MASS_PORDUCTION_LV_CIRCUIT : ItemEntry<ComponentItem> =
         createItem("mass_production_lv_circuit", ComponentItem::create)
             .lang("Mass production LV circuit")
+            .register()
+
+    @Suppress("UNUSED")
+    val UNSOLDERED_MV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
+        createItem("unsoldered_mv_circuit_assembly", ComponentItem::create)
+            .lang("Unsoldered MV Circuit Assembly")
+            .register()
+
+    @Suppress("UNUSED")
+    val MV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
+        createItem("mv_circuit_assembly", ComponentItem::create)
+            .lang("MV Circuit Assembly")
+            .register()
+
+    @Suppress("UNUSED")
+    val MASS_PORDUCTION_MV_CIRCUIT : ItemEntry<ComponentItem> =
+        createItem("mass_production_mv_circuit", ComponentItem::create)
+            .lang("Mass production MV circuit")
+            .register()
+
+    @Suppress("UNUSED")
+    val HV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
+        createItem("hv_circuit_assembly", ComponentItem::create)
+            .lang("HV Circuit Assembly")
+            .register()
+
+    @Suppress("UNUSED")
+    val MASS_PORDUCTION_HV_CIRCUIT : ItemEntry<ComponentItem> =
+        createItem("mass_production_hv_circuit", ComponentItem::create)
+            .lang("Mass production HV circuit")
+            .register()
+
+    @Suppress("UNUSED")
+    val UNSOLDERED_HV_CIRCUIT_ASSEMBLY : ItemEntry<ComponentItem> =
+        createItem("unsoldered_hv_circuit_assembly", ComponentItem::create)
+            .lang("Unsoldered HV Circuit Assembly")
+            .register()
+
+    @Suppress("UNUSED")
+    val WAKFU_SILICON_BOULE : ItemEntry<ComponentItem> =
+        createItem("wakfu_silicon_boule", ComponentItem::create)
+            .lang("Wakfu Silicon Boule")
+            .register()
+
+    @Suppress("UNUSED")
+    val STASIS_SILICON_BOULE : ItemEntry<ComponentItem> =
+        createItem("stasis_silicon_boule", ComponentItem::create)
+            .lang("Stasis Silicon Boule")
+            .register()
+
+    @Suppress("UNUSED")
+    val KROSMOZ_SILICON_BOULE : ItemEntry<ComponentItem> =
+        createItem("krosmoz_silicon_boule", ComponentItem::create)
+            .lang("Krosmoz Silicon Boule")
+            .register()
+
+    @Suppress("UNUSED")
+    val WAKFU_SILICON_WAFER : ItemEntry<ComponentItem> =
+        createItem("wakfu_silicon_wafer", ComponentItem::create)
+            .lang("Wakfu Silicon Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val STASIS_SILICON_WAFER : ItemEntry<ComponentItem> =
+        createItem("stasis_silicon_wafer", ComponentItem::create)
+            .lang("Stasis Silicon Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val KROSMOZ_SILICON_WAFER : ItemEntry<ComponentItem> =
+        createItem("krosmoz_silicon_wafer", ComponentItem::create)
+            .lang("Krosmoz Silicon Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val EXTREME_HI_POW_INTEGRATED_CIRCUIT_WAFER : ItemEntry<ComponentItem> =
+        createItem("extreme_hi_pow_integrated_circuit_wafer", ComponentItem::create)
+            .lang("Extreme Hi-Power Integrated Circuit Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val ULTIMATE_HI_POW_INTEGRATED_CIRCUIT_WAFER : ItemEntry<ComponentItem> =
+        createItem("ultimate_hi_pow_integrated_circuit_wafer", ComponentItem::create)
+            .lang("Ultimatte Hi-Power Integrated Circuit Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val INFINITY_HI_POW_INTEGRATED_CIRCUIT_WAFER : ItemEntry<ComponentItem> =
+        createItem("infinity_hi_pow_integrated_circuit_wafer", ComponentItem::create)
+            .lang("Infinity Hi-Power Integrated Circuit Wafer")
+            .register()
+
+    @Suppress("UNUSED")
+    val EXTREME_HI_POW_INTEGRATED_CIRCUIT : ItemEntry<ComponentItem> =
+        createItem("extreme_hi_pow_integrated_circuit", ComponentItem::create)
+            .lang("Extreme Hi-Power Integrated Circuit")
+            .register()
+
+    @Suppress("UNUSED")
+    val ULTIMATE_HI_POW_INTEGRATED_CIRCUIT : ItemEntry<ComponentItem> =
+        createItem("ultimate_hi_pow_integrated_circuit", ComponentItem::create)
+            .lang("Ultimatte Hi-Power Integrated Circuit")
+            .register()
+
+    @Suppress("UNUSED")
+    val INFINITY_HI_POW_INTEGRATED_CIRCUIT : ItemEntry<ComponentItem> =
+        createItem("infinity_hi_pow_integrated_circuit", ComponentItem::create)
+            .lang("Infinity Hi-Power Integrated Circuit")
             .register()
 
     private fun <T : Item> createItem(
--- src/main/java/me/annwan/acot/qilbycore/data/QilbyMaterials.kt
+++ src/main/java/me/annwan/acot/qilbycore/data/QilbyMaterials.kt
@@ -21,6 +21,7 @@ class QilbyMaterials private constructor(resourceLocation: ResourceLocation) : M
         lateinit var FabricOfReality : Material
         // Early Game Materials
         lateinit var EngineeringAlloy : Material
+        lateinit var SolderingFlux : Material
         fun init() {
             Wakfu = Builder("wakfu")
                 .generateAllParts()
@@ -28,40 +29,44 @@ class QilbyMaterials private constructor(resourceLocation: ResourceLocation) : M
                 .fluid()
                 .color(0x00FFFF).iconSet(MaterialIconSet.SHINY)
                 .blastTemp(4_500, BlastProperty.GasTier.HIGHEST, 3_072)
-                .cableProperties(GTValues.V[GTValues.IV], 3, 0, false)
+                .cableProperties(GTValues.V[GTValues.IV], 5, 0, false)
                 .buildAndRegister()
             Stasis = Builder("stasis")
                 .generateAllParts()
                 .ingot()
+                .fluid()
                 .color(0xA02CBA).iconSet(MaterialIconSet.SHINY)
                 .blastTemp(4_500, BlastProperty.GasTier.HIGHEST, 12_500)
-                .cableProperties(GTValues.V[GTValues.LuV], 3, 0, false)
+                .cableProperties(GTValues.V[GTValues.LuV], 5, 0, false)
                 .buildAndRegister()
+
             DarkMatter = Builder("dark_matter")
                 .generateAllParts()
                 .withTools(40f,100f,125_000,6, 30)
                 .ingot()
                 .fluid()
                 .color(0x204020).iconSet(MaterialIconSet.SHINY)
-                .blastTemp(10_500, BlastProperty.GasTier.HIGHEST,2_000_000)
+                .blastTemp(10_500, BlastProperty.GasTier.HIGHEST,2_000_000,200)
                 .cableProperties(GTValues.V[GTValues.UEV], 4, 32, false)
                 .buildAndRegister()
+
             DarkEnergy = Builder("dark_energy")
                 .generateAllParts()
                 .withTools(60f,120f,250_000,6,30)
                 .ingot()
                 .fluid()
                 .color(0x202040).iconSet(MaterialIconSet.SHINY)
-                .blastTemp(12_000, BlastProperty.GasTier.HIGHEST, 8_000_000)
+                .blastTemp(12_000, BlastProperty.GasTier.HIGHEST, 8_000_000,200)
                 .cableProperties(GTValues.V[GTValues.UIV], 4, 64, false)
                 .buildAndRegister()
+
             RunicStellarite = Builder("runic_stelarite")
                 .generateAllParts()
                 .withTools(80f, 170f, 500_000, 6, 30)
                 .ingot()
                 .fluid()
                 .color(0xD11A1A).iconSet(MaterialIconSet.SHINY)
-                .blastTemp(25_000, BlastProperty.GasTier.HIGHEST, 32_000_000)
+                .blastTemp(25_000, BlastProperty.GasTier.HIGHEST, 32_000_000,200)
                 .cableProperties(GTValues.V[GTValues.UXV], 4, 128, false)
                 .buildAndRegister()
 
@@ -71,7 +76,7 @@ class QilbyMaterials private constructor(resourceLocation: ResourceLocation) : M
                 .ingot()
                 .fluid()
                 .color(0xC79024).iconSet(MaterialIconSet.SHINY)
-                .blastTemp(48_000, BlastProperty.GasTier.HIGHEST, 128_000_000)
+                .blastTemp(48_000, BlastProperty.GasTier.HIGHEST, 128_000_000,200)
                 .cableProperties(GTValues.V[GTValues.OpV], 4, 256, false)
                 .buildAndRegister()
 
@@ -82,7 +87,7 @@ class QilbyMaterials private constructor(resourceLocation: ResourceLocation) : M
                 .ingot()
                 .fluid()
                 .color(0xCF2CAB).iconSet(MaterialIconSet.SHINY)
-                .blastTemp(80_000, BlastProperty.GasTier.HIGHEST, 512_000_000)
+                .blastTemp(80_000, BlastProperty.GasTier.HIGHEST, 512_000_000,200)
                 .cableProperties(GTValues.V[GTValues.MAX], 256, 0, false)
                 .fluidPipeProperties(5_000_000, 500_000,true,true,true,true)
                 .buildAndRegister()
@@ -93,6 +98,11 @@ class QilbyMaterials private constructor(resourceLocation: ResourceLocation) : M
                 .fluid()
                 .color(0xDDEEFF).iconSet(MaterialIconSet.METALLIC)
                 .fluidPipeProperties(400,100,true, false, false, false)
+                .buildAndRegister()
+
+            SolderingFlux = Builder("soldering_flux")
+                .fluid()
+                .color(0xFFFFAA)
                 .buildAndRegister()
 
         }
