9634f6477fef53842b3a6bf4be016398aefde2bf
Annwan <annwan@annwan.me>
2025-09-10 21:20:37 +0200

    Initialisation order AAAAAAAAAAAAAAAAAAAHH

--- src/main/java/fr/qilby/qilby_core/common/block/QilbyCoilBlock.java
+++ src/main/java/fr/qilby/qilby_core/common/block/QilbyCoilBlock.java
@@ -3,13 +3,15 @@ package fr.qilby.qilby_core.common.block;
 import com.gregtechceu.gtceu.api.block.ActiveBlock;
 import com.gregtechceu.gtceu.api.block.ICoilType;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import fr.qilby.qilby_core.QilbyCore;
-import fr.qilby.qilby_core.common.data.QilbyMaterials;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 
+@ParametersAreNonnullByDefault
 public class QilbyCoilBlock extends ActiveBlock {
     public ICoilType coilType;
     public QilbyCoilBlock(Properties properties,ICoilType coilType) {
@@ -18,19 +20,19 @@ public class QilbyCoilBlock extends ActiveBlock {
     }
     public enum CoilType implements StringRepresentable, ICoilType{
         DARK_MATTER_COIL(
-                "dark_matter",12500,8,8,5, QilbyMaterials.DarkMatter, QilbyCore.id("block/coil/dm/machine_coil_dark_matter")
+                "dark_matter",12500,8,8,5, "qilby_core:dark_matter", QilbyCore.id("block/coil/dm/machine_coil_dark_matter")
         ),
         DARK_ENERGY_COIL(
-                "dark_energy",18000,12,8,6, QilbyMaterials.DarkEnergy, QilbyCore.id("block/coil/de/machine_coil_dark_energy")
+                "dark_energy",18000,12,8,6, "qilby_core:dark_energy", QilbyCore.id("block/coil/de/machine_coil_dark_energy")
         ),
         RUNIC_STELLARITE_COIL(
-                "runic_stellarite",22500,12,8,7, QilbyMaterials.RunicStellarite, QilbyCore.id("block/coil/rs/machine_runic")
+                "runic_stellarite",22500,12,8,7, "qilby_core:runic_stellarite", QilbyCore.id("block/coil/rs/machine_runic")
         ),
         STELLARITE_COIL(
-                "stellarite",30000,16,12,8, QilbyMaterials.Stellarite, QilbyCore.id("block/coil/s/machine_stellarite")
+                "stellarite",30000,16,12,8, "qilby_core:stellarite", QilbyCore.id("block/coil/s/machine_stellarite")
         ),
         THETA_COIL(
-                "infinity",50000,32,16,9, QilbyMaterials.FabricOfReality, QilbyCore.id("block/coil/t/machine_theta")
+                "infinity",50000,32,16,9, "qilby_core:fabric_of_reality", QilbyCore.id("block/coil/t/machine_theta")
         );
         @NotNull
         private final String name;
@@ -48,21 +50,30 @@ public class QilbyCoilBlock extends ActiveBlock {
         public final int getEnergyDiscount() {return energyDiscount;}
         private final int tier;
         public int getTier() {return tier;}
+        private Material material;
+        private final String materialId;
         @NotNull
-        private final Material material;
-        @NotNull
-        public Material getMaterial() {return material;}
+        public Material getMaterial() {
+            /*
+             initialization order is the bane of my existance :despair:
+             — jurrejelle, GTM dev, 2025
+            */
+            if (material == null)
+                material = GTMaterials.get(materialId);
+            return material;
+
+        }
         private final ResourceLocation texture;
         public ResourceLocation getTexture() {return texture;}
 
-        CoilType(@NotNull String name, int coilTemperature, int level, int energyDiscount, int tier, @NotNull Material material,
+        CoilType(String name, int coilTemperature, int level, int energyDiscount, int tier, String materialId,
                  ResourceLocation texture) {
             this.name = name;
             this.coilTemperature = coilTemperature;
             this.level = level;
             this.energyDiscount = energyDiscount;
             this.tier = tier;
-            this.material = material;
+            this.materialId = materialId;
             this.texture = texture;
         }
         @NotNull
